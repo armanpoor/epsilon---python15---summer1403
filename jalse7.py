60fe6dc74cb3e02085d379b9c505fef33f14f2c6
armanpoor <105088202+armanpoor@users.noreply.github.com>
2024-07-22 18:21:55 +0330

    Add files via upload

---
+++ jalse7.py
@@ -0,0 +1,29 @@
+# indent
+# if 5>2:
+#     print("5 از 2 بزرگ تر است")
+#     if 8>2:
+#         print("10 az 20 koochektar ast")      
+# if 10<20:
+#     print("10 az 20 koochektar ast")
+    
+    
+#comemnt
+
+#variables
+# a = 4 # این متغیر عدد 4 را در خود ذخیره می کند
+
+# b = "ali" # این متغییر ....
+
+# print(a , b)
+
+#input
+# esm = input("lotfan esm koochak khodet ro benevis azizm: ")
+# family = input("hala famito behem begoo: ")
+# print(esm, family)
+
+#casting
+addad = input("addad khod ra vered konid: ")
+addad = int(addad)
+print(type(addad))
+final = addad + 10
+print(final)
