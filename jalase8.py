e21aa9ba067132a0e91c023b413ef879d712649e
armanpoor <105088202+armanpoor@users.noreply.github.com>
2024-07-27 10:52:33 +0330

    Add files via upload

---
+++ jalase8.py
@@ -0,0 +1,26 @@
+# #tamrin1
+
+# zaman = int(input("Sara Alan Sa'at Chande: "))
+
+# if zaman>0 and zaman<6:
+#     print("Nime Shab Bekheyr!")
+
+# elif zaman>6 and zaman<12:
+#     print("Sobh Bekheyr!")
+    
+# elif zaman>12 and zaman<18:
+#     print("Ba'ad Az Zohr Bekheyr!")
+    
+# elif zaman>18 and zaman<24:
+#     print("Shab Bekheyr!")
+# else:
+#     print("Sara khanoom loftn addy dar bazeye 0 ta 24 vared konid!")
+
+#boolean
+# ما تا الان تایپ های استرینگ و اینتیجر را یاد گرفتیم
+# a = 10<2
+# print(type(a))
+# print(a)
+
+#operators
+#https://www.w3schools.com/python/python_operators.asp
